918f8f4d0cb19732117d9d966418c00087557d85
lfdario <155621762+lfdario@users.noreply.github.com>
2025-10-23 09:27:40 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://lfdario.github.io/site-automation-pro/',
-  base: '/site-automation-pro/',
+  site: 'https://lfdario.github.io/motori/', // ✅
+  base: '/motori/',                          // ✅
   integrations: [tailwind(), sitemap()],
   output: 'static'
 });
