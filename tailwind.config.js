61e57daf9eb2967dced22e8c95a5ca3ed11daaca
lfdario <155621762+lfdario@users.noreply.github.com>
2025-10-23 11:02:16 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
-  theme: { extend: { container: { center: true, padding: "1rem" } } },
-  plugins: [],
-};
